625bd426747625c13cace931a80d6f657e7dff37
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-10-22 15:45:06 -0400

    fix: fix tailwind css not applying

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import "./App.css";
 import Heading from "./components/Heading";
 import TextSphere from "./components/TextSphere";
 
 function App() {
   return (
     <>
-      <Heading />
-      <TextSphere />
+      <div className="container ">
+        <div className=" h-96">
+          <Heading />
+        </div>
+        <div className="ml-96">
+          <TextSphere />
+        </div>
+      </div>
     </>
   );
 }
