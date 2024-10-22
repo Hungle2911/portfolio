69d8108acccf0dfbe0d09f3c4383dc8272fe420a
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-10-22 14:04:33 -0400

    feat: add heading

---
+++ src/components/Heading.tsx
@@ -0,0 +1,20 @@
+function Heading() {
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-8xl font-bold font-mono mb-4 inline-block">
+        WILLIAM_LE.DEV
+      </h1>
+      <p className="text-2xl font-mono mt-8 max-w-2xl">
+        Having recently completed a comprehensive web development bootcamp, I
+        have acquired skills in HTML, CSS, JavaScript, React, and Node.js, among
+        other technologies. I aspire to work on projects that tackle real-world
+        challenges. My goal is to contribute to innovative solutions that make a
+        positive impact on society, whether through improving healthcare
+        systems, enhancing environmental sustainability, or increasing access to
+        education through technology.
+      </p>
+    </div>
+  );
+}
+
+export default Heading;
