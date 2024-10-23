12a850f6701f6ac2f621d63ec8e64d3ca4945638
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-10-23 11:15:05 -0400

    change welcome text

--- src/components/TerminalLine.tsx
+++ src/components/TerminalLine.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const TerminalLine = () => {
   const terminalText =
-    "Welcome to my terminal-like interface built with React, Tailwind, and TypeScript.";
+    "Hi! Thank you for taking your time to look at this portfolio. I am Billy, your virtual assistant. Feel free to ask me anything related to William. For example, you can ask me about his projects, skills, or even his contact info. I am here to help you! For a list of available commands, type `help`";
   const words = terminalText.split(" ");
   const [displayedText, setDisplayedText] = useState<string[]>([]);
   const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
@@ -12,7 +12,7 @@ const TerminalLine = () => {
       const timeout = setTimeout(() => {
         setDisplayedText((prev) => [...prev, words[currentWordIndex]]);
         setCurrentWordIndex((prevIndex) => prevIndex + 1);
-      }, 300);
+      }, 200);
       return () => clearTimeout(timeout);
     }
   }, [currentWordIndex, words]);
