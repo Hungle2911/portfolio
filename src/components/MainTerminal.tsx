d15cf7caa77516fda59fbaae9bef3d28a62a5b19
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-10-23 17:11:34 -0400

    handle command input

--- src/components/MainTerminal.tsx
+++ src/components/MainTerminal.tsx
@@ -1,15 +1,30 @@
 import { FormEvent, useState } from "react";
+import { commands } from "./commands/commands";
 
 const MainTerminal = () => {
   const [input, setInput] = useState<string>("");
+
+  const handleCommand = (cmd: string) => {
+    const trimmedCmd = cmd.trim().toLowerCase();
+    if (trimmedCmd === "") return;
+
+    const command = commands[trimmedCmd];
+    if (command) {
+      command.execute();
+    } else {
+      return `Command not found: ${trimmedCmd}. Type 'help' for available commands.`;
+    }
+  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const trimmedInput = input.trim();
-
+    const trimmedInput = input.trim().toLowerCase();
+    handleCommand(trimmedInput);
     if (trimmedInput !== "") {
       setInput("");
     }
   };
+
   return (
     <main>
       <form className="" onSubmit={handleSubmit}>
@@ -19,6 +34,7 @@ const MainTerminal = () => {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           className="bg-transparent text-white outline-none"
+          autoFocus
         />
       </form>
     </main>
